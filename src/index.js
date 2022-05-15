d0c36575f1a8e5bd9426818f9092e71ee2c71b1e
rammfall <15bogdan17@gmail.com>
2022-05-16 00:16:22 +0300

    Update versions

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ const fastify = Fastify({
   logger: true,
 });
 
-fastify.register(import('fastify-cookie'));
-fastify.register(import('fastify-multipart'), {
+fastify.register(import('@fastify/cors'));
+fastify.register(import('@fastify/multipart'), {
   addToBody: true,
 });
-fastify.register(import('fastify-cors'));
+fastify.register(import('@fastify/cookie'));
 
 fastify.get('/hello', (request, reply) => {
   return reply.send('world');
