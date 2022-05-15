7776a071517ca00f786b5e3afdf48ef36804d90f
rammfall <15bogdan17@gmail.com>
2022-05-15 23:43:44 +0300

    Add plugins

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ const fastify = Fastify({
   logger: true,
 });
 
+fastify.register(import('fastify-cookie'));
+fastify.register(import('fastify-multipart'), {
+  addToBody: true,
+});
+fastify.register(import('fastify-cors'));
+
 fastify.get('/hello', (request, reply) => {
   return reply.send('world');
 });
